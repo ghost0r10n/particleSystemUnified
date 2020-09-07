b24ee410b4abf3bdf0ddc36fc269666d655331fa
ghost0r10n <33174442+ghost0r10n@users.noreply.github.com>
2020-09-07 16:20:05 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,32 @@
 let particles = [];
 let distances = [];
 let time;
+let backImage;
+let pointImage;
+
+function preload(){
+    backImage = loadImage("assets/back1.jpeg")
+    pointImage = loadImage("assets/point.png")
+}
+
 function setup() {
     createCanvas(600,600);
     time = 0;
-    smooth();
-    filter(GRAY)
+  noCursor();
+   
 }
 
 function draw() {
-    background(0);
-     strokeWeight(1);
     
+    background(0);
+    
+    image(backImage,-20,-20,width,height)
+    
+    stroke(255,115,0)
+    strokeWeight(0.5);
+    if(!mouseIsPressed){
+    text("Click here",mouseX,mouseY)
+    }
     translate(width/2,height/2)
    
     if(time<200){
@@ -74,10 +89,11 @@ class Particle{
             this.d.y*=-1
         }
          noFill()
-     //    stroke(255,115,0,particles[i].d.x+particles[i].d.y);
+         stroke(255,115,0,particles[i].d.x+particles[i].d.y);
    // rect(this.pos.x-50,this.pos.y-50,100,100)
         fill(255,115,0,this.d.x+this.d.y);
-        ellipse(this.pos.x,this.pos.y,1,1);
+       // ellipse(this.pos.x,this.pos.y,1,1);
+        image(pointImage,this.pos.x-2.5,this.pos.y-2.5,5,5)
         
     }
  
@@ -88,6 +104,8 @@ class Particle{
                this.pos = createVector(random(-100,100),random(-100,100));
                this.velocity = createVector(random(-1,1),random(-1,1))
             }
+        
+        if(!mouseIsPressed){
         
         this.pos.add(this.velocity);
          if(mouseX-300>=this.pos.x-50&&mouseY-300>=this.pos.y-50){
@@ -130,6 +148,19 @@ class Particle{
                  }
              }
          }
+        
+        
+        
+        }else{
+             var mouse = createVector(mouseX-300,mouseY-300);
+    var acceleration = p5.Vector.sub(mouse,this.pos);
+    // Set magnitude of acceleration
+    acceleration.setMag(0.2);
+
+    this.velocity.add(acceleration);
+ //   this.velocity.limit(this.topspeed);
+    this.pos.add(this.velocity);
+        }
        
          }          
                 
